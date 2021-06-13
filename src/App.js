7c93082809de45eb9ccc64064c4c64f4a4af442c
Gokulakrishnan <glapskrish@gmail.com>
2021-06-13 08:27:09 +0530

    updated the data

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
 import {React,useState, useEffect} from 'react';
 import Player from './components/Player';
 
+import data from './data.json';
 
 function App() {
-const [songs, setSongs] = useState([
-	{
-	 title:"Solo",
-	 artist:"Gls",
-	 img_src:"./images/image1.jpeg",
-	 src:"./music/Aigiri-Nandini.mp3"
-	},
-  {
-    title:"Anbe Sivam",
-    artist:"Gls",
-    img_src:"./images/image2.jpg",
-    src:"./music/Anbe-Sivam.mp3"
-   },{
-    title:"Uriyadi",
-    artist:"Gls",
-    img_src:"./images/image3.jpg",
-    src:"./music/Agni-Kunjondru.mp3"
-   },{
-    title:"Ayan",
-    artist:"Gls",
-    img_src:"./images/image4.jpg",
-    src:"./music/OhSupernova.mp3"
-   },{
-    title:"Billa 2",
-    artist:"Gls",
-    img_src:"./images/image5.jpg",
-    src:"./music/Unakkulle-Mirugam.mp3"
-   },{
-    title:"Charlie Chaplin",
-    artist:"Gls",
-    img_src:"./images/image6.jpg",
-    src:"./music/Charlie-Chaplin.mp3"
-   }
-])
+const [songs, setSongs] = useState(data)
   const[currentSongIndex,setCurrentSongIndex]=useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex+1);
 
